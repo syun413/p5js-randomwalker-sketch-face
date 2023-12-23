5bc5216777c7a48f00aec58405d86fc15e7b252d
Syun <b10505044@ntu.edu.tw>
2023-12-23 19:43:51 +0800

    Fixed parameter & add more walker

--- random_walker_grid.js
+++ random_walker_grid.js
@@ -1,7 +1,7 @@
 let edgeImage;
 let walkers = [];
 let edgePoints = [];
-const numWalkers = 100; // More walkers for a denser drawing
+const numWalkers = 1000; // Increased number of walkers for faster drawing
 
 function preload() {
   edgeImage = loadImage('teacher_edges.png'); // Make sure the path is correct
@@ -9,15 +9,19 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0); // Draw the background only once if you want to preserve the trails
+  background(0);
   edgeImage.loadPixels();
+
+  // Calculate average brightness and set dynamic threshold
+  let avgBrightness = calculateAverageBrightness(edgeImage);
+  let threshold = avgBrightness * 0.5; // Set threshold as 50% of average brightness
 
   // Identify edge points and store their positions
   for (let x = 0; x < edgeImage.width; x++) {
     for (let y = 0; y < edgeImage.height; y++) {
       let index = (x + y * edgeImage.width) * 4;
       let brightness = (edgeImage.pixels[index] + edgeImage.pixels[index + 1] + edgeImage.pixels[index + 2]) / 3;
-      if (brightness > 128) { // Threshold can be adjusted
+      if (brightness > threshold) {
         edgePoints.push(createVector(x, y));
       }
     }
@@ -30,52 +34,60 @@ function setup() {
 }
 
 function draw() {
-  // Comment or remove this line to stop the canvas from getting progressively brighter
-  // background(255, 255, 255, 5);
-
   for (let i = 0; i < walkers.length; i++) {
     walkers[i].move();
     walkers[i].display();
   }
-
-  // Termination condition (example: stop after a certain number of frames)
-  // if (frameCount > maxFrames) {
-  //   noLoop(); // This stops the p5.js draw loop
-  // }
 }
 
 class Walker {
   constructor(x, y) {
     this.pos = createVector(x, y);
     this.vel = p5.Vector.random2D();
+    this.maxSpeed = 10; // Increased maximum speed
   }
 
   move() {
-    let closestEdge = null;
-    let record = Infinity;
+    let closestEdge = findClosestEdge(this.pos);
 
-    // Find closest edge point
-    for (let i = 0; i < edgePoints.length; i++) {
-      let d = p5.Vector.dist(this.pos, edgePoints[i]);
-      if (d < record) {
-        record = d;
-        closestEdge = edgePoints[i];
-      }
-    }
-
+    // Move towards the closest edge point
     if (closestEdge) {
-      let force = p5.Vector.sub(closestEdge, this.pos);
-      force.setMag(1); // Increase the force magnitude for a stronger attraction
-      this.vel.add(force);
+      let desired = p5.Vector.sub(closestEdge, this.pos);
+      desired.setMag(this.maxSpeed);
+      this.vel.lerp(desired, 0.2); // Use lerp for smoother transition of velocities
+      this.vel.limit(this.maxSpeed);
     }
 
-    this.vel.limit(5); // Limit the velocity to prevent too fast movement
     this.pos.add(this.vel);
   }
 
   display() {
-    stroke(255); // Make the walkers white to be visible on the black background
+    stroke(255);
     strokeWeight(1);
     point(this.pos.x, this.pos.y);
   }
+}
+
+// Function to calculate average brightness of the image
+function calculateAverageBrightness(img) {
+  let totalBrightness = 0;
+  for (let i = 0; i < img.pixels.length; i += 4) {
+    totalBrightness += (img.pixels[i] + img.pixels[i + 1] + img.pixels[i + 2]) / 3;
+  }
+  return totalBrightness / (img.width * img.height);
+}
+
+// Function to find the closest edge to a given position
+function findClosestEdge(position) {
+  let recordDistance = Infinity;
+  let closestEdge = null;
+
+  for (let edge of edgePoints) {
+    let d = p5.Vector.dist(position, edge);
+    if (d < recordDistance) {
+      recordDistance = d;
+      closestEdge = edge;
+    }
+  }
+  return closestEdge;
 }
